c6b4faa47a4e0fa5cead04fd4251c5c769db4961
lucasFilppe <lucasfilipe2323@icloud.com>
2024-11-01 17:36:32 -0300

    tamanho logo dispositivos menores

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -8,7 +8,7 @@ export default function CosaLogo() {
         alt="Acme Logo"
         width={50}
         height={50}
-        className="rounded-full object-cover w-40 h-40 md:w-32 md:h-32 lg:w-24 lg:h-24"
+        className="rounded-full object-cover w-32 h-32 md:w-32 md:h-32 lg:w-24 lg:h-24"
         priority
       />
     </div>
