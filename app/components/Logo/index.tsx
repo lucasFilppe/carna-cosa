001c88e2013e30ec6a8b91e3f22da75f2efddd5f
lucasFilppe <lucasfilipe2323@icloud.com>
2024-11-01 17:14:46 -0300

    tamanho logo

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -6,8 +6,8 @@ export default function CosaLogo() {
       <Image
         src="/cosalogo.jpeg"
         alt="Acme Logo"
-        width={50}
-        height={50}
+        width={30}
+        height={30}
         className="rounded-full object-cover w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40"
         priority
       />
